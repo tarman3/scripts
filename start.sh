3a77a01d9723747776e7dc3f71423e7d0dd77a2a
tarman3 <joice501790018@tutamail.com>
2026-03-23 08:40:49 +0200

    update

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-hours=`date +"%H"`
-if [ $hours -lt 7 ]; then
-    brightnessctl -q set 10%
-fi
-
 power_state=`cat /sys/class/power_supply/ACAD/online`
 if [ ${power_state} = 0 ]; then
     nmcli radio wifi off
@@ -16,23 +11,20 @@ fi
 
 srm -rll '/home/user/.cache/chromium'
 srm -rll '/home/user/.config/chromium'
-
 srm -rll '/home/user/.cache/librewolf'
-
 srm -rll '/home/user/.local/share/materialgram/tdata/user_data'
+
+if [ `date +"%H"` -lt 7 ]; then brightnessctl -q set 10%; fi
 
 # brightnessctl -q set 95%
 plasma-apply-wallpaperimage '/home/user/Pictures/arch.png'
 
 sleep 3
 
-hours=`date +"%H"`
-if [ $hours -gt 7 ]; then
+if [ `date +"%H"` -gt 7 ]; then
     uv run --project $HOME/uv/python13 $HOME/.local/scripts/apod.py &
 fi
 
-# killall conky
-# sleep 10
 conky &
 
 exit
